6e06a8273b806d41c375883bb469dd2ff9eb79bc
manojconcept <manojveerapavu@gmail.com>
2024-03-16 10:18:16 +0530

    init

---
+++ src/components/Global/Global.js
@@ -0,0 +1,1 @@
+export const API = "https://65aca4a1adbd5aa31bdf63e7.mockapi.io/library";
---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="img">
+      <h1 className="homeHeading animate-character">
+        One best book is equal to hundred good friends but
+      </h1>
+      <h1 className="homeHeading1 animate-character">
+        One good friend is equal to a Library...
+      </h1>
+      <h1 className="homeHeading2 animate-character">
+        => Dr.A.P.J.Abdul Kalam
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
